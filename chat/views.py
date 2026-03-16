736a46fee52004a32a2723226058672511b5aef3
asukim <asukim2020@gmail.com>
2026-03-16 12:19:26 +0900

    컴파일 오류 수정

--- chat/views.py
+++ chat/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Room, RoomMember
-from .serializers import InviteSerializer
 
 User = get_user_model()
 
